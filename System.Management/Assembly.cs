2f4fef0ee3a1175a54d0ac620b89a4753d735591
rreminy <rreminy@gmail.com>
2023-06-04 03:45:10 +0000

    Update SonarDistrib

--- System.Management/Assembly.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Minor Code Smell", "S2292:Trivial properties should be auto-implemented", Justification = "<Pending>")]
-[assembly: SuppressMessage("Minor Code Smell", "S1905:Redundant casts should not be used", Justification = "<Pending>")]
